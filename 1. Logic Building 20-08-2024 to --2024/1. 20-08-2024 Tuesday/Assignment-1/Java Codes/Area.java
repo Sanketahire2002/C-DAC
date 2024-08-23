310d6e27b2ad604d3fdbab0e1fcedb354d42c57b
Sanketahire2002 <101521154+Sanketahire2002@users.noreply.github.com>
2024-08-23 08:56:41 +0530

    Add files via upload

---
+++ 1. Logic Building 20-08-2024 to --2024/1. 20-08-2024 Tuesday/Assignment-1/Java Codes/Area.java
@@ -0,0 +1,14 @@
+class SquareArea {
+    public static void main(String[] args) 
+    {
+        int side = 5;
+        int L = 2;
+        int B = 6;
+
+        int area = side * side;
+
+        System.out.println("The area of the square is: " + area);
+
+        System.out.println("The area of the rectangle is: " + (L*B));
+    }
+}
