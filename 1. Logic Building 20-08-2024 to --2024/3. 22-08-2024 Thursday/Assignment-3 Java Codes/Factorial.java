5661f61545344e6ab6d5c5f070e4b73ffc87920d
Sanketahire2002 <101521154+Sanketahire2002@users.noreply.github.com>
2024-08-24 22:40:22 +0530

    Add files via upload

---
+++ 1. Logic Building 20-08-2024 to --2024/3. 22-08-2024 Thursday/Assignment-3 Java Codes/Factorial.java
@@ -0,0 +1,14 @@
+class Factorial 
+{
+    public static void main(String[] args) 
+    {
+        int number = 10;
+        int factorial = 1;
+
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("The factorial of 10 is: " + factorial);
+    }
+}
