d5dfb7d835c65d2f847d5855b077a97a85c7a156
Sanketahire2002 <101521154+Sanketahire2002@users.noreply.github.com>
2024-09-13 19:27:01 +0530

    Delete 3. OOPJ Day-3 05-09-2024 directory

--- 3. OOPJ Day-3 05-09-2024/Assignment-2/CoreJava/J/com.java
+++
@@ -1,8 +0,0 @@
-class Demo
-{
-	public static void main(String[] args)
-	{
-		for(int i=0;i<args.length;i++)
-			System.out.print("\t"+args[i]);
-	}
-}
--- 3. OOPJ Day-3 05-09-2024/Assignment-2/CoreJava/test.java
+++
@@ -1,9 +0,0 @@
-package com.tcs.loan.carloan;
-
-public class test 
-{
-	public static void main(String[] args)
-	{
-		System.out.print("Hello World. Packages.");
-	}
-}
